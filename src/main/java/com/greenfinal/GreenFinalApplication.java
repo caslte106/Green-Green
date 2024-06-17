ae5b870bd233a4a373c1bd29bb8589a40e417154
HoSung <khsung106@gmail.com>
2024-06-17 12:19:37 +0900

    first commit

---
+++ src/main/java/com/greenfinal/GreenFinalApplication.java
@@ -0,0 +1,13 @@
+package com.greenfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenFinalApplication.class, args);
+	}
+
+}
